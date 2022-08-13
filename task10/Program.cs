5515db92c0dd9932ee5cec2d3e8d6104a25149f0
Art <asgrafov82@gmail.com>
2022-08-13 17:08:43 +0300

    task 10

---
+++ task10/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.WriteLine("Input number :");
+
+if (int.TryParse(Console.ReadLine(), out int n))
+{
+    if (n > 99 & n < 1000)
+    {
+        Console.WriteLine(n % 100 / 10);
+    }
+    else
+    {
+        Console.WriteLine("Not three-digit number");
+    }   
+}
+else
+{
+    Console.WriteLine("Incorrect input");
+}
