4a10a1ecfbde4d46f50afc58c156e1870a1ac12d
Art <asgrafov82@gmail.com>
2022-08-14 00:00:40 +0300

    task 23

---
+++ task23/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+int n, i = 1;
+
+try
+{
+    Console.Write("Input number : ");
+    n = int.Parse(Console.ReadLine() ?? "");
+    while (i <= n)
+    {
+    Console.Write(Math.Pow(i, 3) + " ");
+    i++;
+    }
+}
+catch (Exception exception)
+{
+    Console.WriteLine($"Error : {exception.Message}");
+}
