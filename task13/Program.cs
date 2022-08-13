a01ee1239b1eded85b82413cb5b302b961854b96
Art <asgrafov82@gmail.com>
2022-08-13 17:22:45 +0300

    task 13

---
+++ task13/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.WriteLine("Input number :");
+
+if (int.TryParse(Console.ReadLine(), out int n))
+{
+    if (n > 99 & n < 1000)
+    {
+        Console.WriteLine(n % 10);
+    }
+    else
+    {
+        Console.WriteLine("The third digit is missing");
+    }   
+}
+else
+{
+    Console.WriteLine("Incorrect input");
+}
