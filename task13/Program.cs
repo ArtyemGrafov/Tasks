dee9fa562608147d1fa68f56f448b02115e53c83
Art <asgrafov82@gmail.com>
2022-08-13 17:40:00 +0300

    task 13 mod

--- task13/Program.cs
+++ task13/Program.cs
@@ -4,8 +4,12 @@ Console.WriteLine("Input number :");
 
 if (int.TryParse(Console.ReadLine(), out int n))
 {
-    if (n > 99 & n < 1000)
+    if (n > 99)
     {
+        while (n > 1000) {
+            n = n / 10;
+        }
+        
         Console.WriteLine(n % 10);
     }
     else
