87ecb692751637c3867aa95a34403dc4102df901
Art <asgrafov82@gmail.com>
2022-08-13 17:32:05 +0300

    task 15

---
+++ task15/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.WriteLine("Input number :");
+
+if (int.TryParse(Console.ReadLine(), out int n))
+{
+    if (n > 0 & n < 6)
+    {
+        Console.WriteLine("NO");
+    }
+    else if (n > 5 & n < 8)
+    {
+        Console.WriteLine("YES");
+    }
+    else Console.WriteLine("Not in range");
+}
+else
+{
+    Console.WriteLine("Incorrect input");
+}
