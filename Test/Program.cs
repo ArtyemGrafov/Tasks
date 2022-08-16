3eb775486a479a2e055e955560cb1aec59cd2c75
Art <asgrafov82@gmail.com>
2022-08-16 21:39:10 +0300

    Test

---
+++ Test/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+int num = getInt();
+
+int n = getLength(num);
+
+int getInt()
+{
+    Console.Write("Введи число : ");
+    while(true)
+    {
+        if (int.TryParse(Console.ReadLine(), out int result))
+            return result;
+        Console.WriteLine("Error");
+    }
+}
+
+int getLength(int n)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        ++result;
+    }
+
+    return result;
+}
+
+Console.WriteLine($"Количество цифр = {n}");
