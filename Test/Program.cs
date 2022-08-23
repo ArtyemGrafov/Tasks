f11f056509e989390615929dc862fcf6b48d60c9
Art <asgrafov82@gmail.com>
2022-08-23 22:00:13 +0300

    test

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,50 +1,90 @@
 ﻿Console.Clear();
 
-int len = getLength(getInt());
+// int a = GetInt("Input a : ");
+// int b = GetInt("Input b : ");
+// int c = GetInt("Input c : ");
 
-int getInt()
-{
-    Console.Write("Введи число : ");
-    while(true)
-    {
-        if (int.TryParse(Console.ReadLine(), out int result))
-            return result;
-        Console.WriteLine("Error");
-    }
-}
+// if (a < b + c & b < a + c & c < a + b) Console.Write("Yes");
+// else Console.Write("NO");
 
-int getLength(int n)
-{
-    int result = 0;
-    while (n > 0)
-    {
-        n /= 10;
-        ++result;
-    }
 
-    return result;
-}
+// int a1 = 0;
+// int a2 = 1;
 
-Console.WriteLine($"Количество цифр = {len}");
+// int i = Convert.ToInt32(n);
 
-// int len = Factorial(getInt());
+// String s = Convert.ToString(i, 2);
+// Console.WriteLine(s);
 
-// int getInt()
+// String res = String.Empty;
+
+// do 
 // {
-//     Console.Write("Введи число : ");
-//     while(true)
-//     {
-//         if (int.TryParse(Console.ReadLine(), out int result))
-//             return result;
-//         Console.WriteLine("Error");
-//     }
+//     //Console.Write(n % 2);
+//     res += n % 2;
+//     n /= 2;
+// }
+// while (n > 0);
+
+// Console.Write(res.Reverse().ToArray());
+
+// Console.Write(a1 + " " + a2 + " ");
+
+// for (int i = 0; i < n - 2; i++)
+// {
+//     int a3 = a1 + a2;
+//     Console.Write(a3 + " ");
+//     a1 = a2;
+//     a2 = a3;
 // }
 
-// int Factorial(int n)
-// {
-//     if (n == 1) return 1;
- 
-//     return n * Factorial(n - 1);
-// }
 
-// Console.WriteLine($"Факториал = {len}");
+
+int n = GetInt("Input n : ");
+
+int[] array = GetRandomArray(n, 0, 100);
+
+int[] arrayCopy = CopyArray(array);
+
+//array[0] = 1000;
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"[{string.Join(", ", arrayCopy)}]");
+array[0] = 1000;
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+
+int[] CopyArray(int[] arr)
+{
+    int[] res = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        res[i] = arr[i];
+    }
+    return res;
+}
+
+int[] GetRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+
+    Random random = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = random.Next(min, max + 1);
+    }
+    return array;
+}
+
+int GetInt(string msg)
+{
+    Console.Write(msg);
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out int num))
+            return num;
+        Console.WriteLine("Ошибка ввода");
+        Console.Write(msg);
+    }
+}
