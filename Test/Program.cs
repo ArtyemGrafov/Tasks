542481ab85036e0100f39f60863e7e2acba81da4
Art <asgrafov82@gmail.com>
2022-09-02 21:38:49 +0300

    test

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,105 +1,36 @@
-﻿Console.Clear();
+﻿// Console.Clear();
+// Console.Write("Enter n : ");
+// int n = int.Parse(Console.ReadLine() ?? "");
+// Console.Write("Enter m : ");
+// int m = int.Parse(Console.ReadLine() ?? "");
 
-int n = GetInt("Input n : ");
-int m = GetInt("Input m : ");
+// Console.WriteLine("Pow : " + GetPow(n, m));
 
-// if (m == n)
+// int GetPow(int num, int rank)
 // {
-    int[,] array = GetRandomArray(n, m, 1, 9);
-    PrintArray(array);
-    Console.WriteLine();
-    ModArray(array);
-    PrintArray(array);
-// }
-// else
-// {
-//     Console.WriteLine("Mission unimposible");
+//     if (rank == 0) return 1;
+//     if (rank == 1) return num;
+//     if (num == 0) return 0;
+//     return num * GetPow(num, rank - 1);
 // }
 
+// Console.WriteLine("Sum digits : " + GetSumDigits(n));
 
-void PrintArray(int[,] arr)
-{
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            Console.Write(arr[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-}
-
-// void ModArray(int[,] arr)
+// int GetSumDigits(int num)
 // {
-//     for (int i = 0; i < arr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < i; j++)
-//         {
-//             if (i != j)
-//             {
-//                 int tmp = arr[i, j];
-//                 arr[i, j] = arr[j, i];
-//                 arr[j, i] = tmp;
-//             }
-//         }
-//     }
+//     if (num / 10 == 0) return num % 10;
+//     else 
+//         return GetSumDigits(num % 10) + GetSumDigits(num / 10);
 // }
 
-void ModArray(int[,] arr)
-{
-    for (int i = 0; i < arr.GetLength(1); i++)
-    {
-        int tmp = arr[0, i];
-        arr[0, i] = arr[arr.GetUpperBound(0), i];
-        arr[arr.GetUpperBound(0), i] = tmp;
-    }
-}
 
 
-//int[] arrayCopy = CopyArray(array);
+// int a = 5; int b = 15;
 
-//array[0] = 1000;
+// Console.Write(GetPrint(a, b));
 
-// Console.WriteLine($"[{string.Join(", ", array)}]");
-// Console.WriteLine($"[{string.Join(", ", arrayCopy)}]");
-// array[0] = 1000;
-// Console.WriteLine($"[{string.Join(", ", array)}]");
-
-
-// int[] CopyArray(int[] arr)
+// string GetPrint(int start, int end) 
 // {
-//     int[] res = new int[arr.Length];
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         res[i] = arr[i];
-//     }
-//     return res;
+//     if (start == end) return end.ToString();
+//     return start + ", " + GetPrint(start + 1, end);
 // }
-
-int[,] GetRandomArray(int n, int m, int min, int max)
-{
-    int[,] array = new int[n, m];
-
-    Random random = new Random();
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            array[i, j] = random.Next(min, max + 1);
-        }
-    }
-    return array;
-}
-
-int GetInt(string msg)
-{
-    Console.Write(msg);
-    while (true)
-    {
-        if (int.TryParse(Console.ReadLine(), out int num))
-            return num;
-        Console.WriteLine("Ошибка ввода");
-        Console.Write(msg);
-    }
-}
